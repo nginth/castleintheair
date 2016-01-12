f5319cbb2971637ebf22678ec7ca8b41c198e5ed
Nick Ginther <nginth@gmail.com>
2016-01-12 01:50:18 -0600

    added ugly great depression chart

---
+++ src/js/djiHistoryChart.js
@@ -0,0 +1,35 @@
+var START_DATE = 1929;
+var END_DATE = 1945;
+
+$.getJSON('/static/data.json', makeDJIChart);
+
+function makeDJIChart(djiJSON, callback){
+    var djiData = {
+        labels: [],
+        datasets:[
+            {
+                fillColor: "rgba(220,220,220,0.2)",
+                strokeColor: "rgba(220,220,220,1)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: []
+            }
+        ]
+    };
+
+    $.each(djiJSON, function(index, element){
+        var date = parseInt(element.date.split("-")[0]);
+        if(date >= START_DATE &&  date <= END_DATE){
+            djiData.labels.push(element.date);
+            djiData.datasets[0].data.push(element.avg);
+        }
+    });
+    
+    var djiContext = $("#dji").get(0).getContext("2d");
+    var djiChart = new Chart(djiContext).Line(djiData, {
+        datasetStrokeWidth: 3
+    });
+
+}
