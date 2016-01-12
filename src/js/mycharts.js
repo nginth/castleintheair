3dee2aba7917ea53898e4c13aae3ebf69934aea8
Nick Ginther <nginth@gmail.com>
2016-01-11 20:17:45 -0600

    added randomize button

--- src/js/mycharts.js
+++ src/js/mycharts.js
@@ -1,13 +1,38 @@
 var data = {
-  // A labels array that can contain any sort of values
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  // Our series array that contains series objects or in this case series data arrays
-  series: [
-    [5, 2, 4, 2, 0]
-  ]
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
 };
 
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
-new Chartist.Line('.ct-chart', data);
+var context = $("#chart1").get(0).getContext("2d");
+var chart1 = new Chart(context).Line(data, {
+    datasetStrokeWidth: 3
+});
+
+function randomize(){
+    var data = chart1.datasets[0].points;
+    data.forEach(function(element, index, array){
+        element.value = Math.random() * 100;
+    });
+    chart1.update();
+ }
